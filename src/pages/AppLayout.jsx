7674ada33086f356efd06c101594ff4e8a11735b
BogdanSFG <151773847+BogdanSFG@users.noreply.github.com>
2025-11-10 19:16:42 +0200

    initial commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import AppNav from "../components/AppNav";
+function AppLayout() {
+  return (
+    <div>
+      <AppNav></AppNav>
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
